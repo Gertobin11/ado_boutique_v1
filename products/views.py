09b3b94808e6f887a4315156d28fea2ffb5cddfd
Gertobin11 <gertobin11@gmail.com>
2021-08-01 21:26:53 +0000

    added jquery functionality to the sort selector box

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from .models import Product, Category
 from django.db.models import Q
+from django.db.models.functions import Lower
 
 
 def all_products(request):
@@ -12,7 +13,7 @@ def all_products(request):
     categories = None
     sort = None
     direction = None
-    sortkey = 'category_id'
+    sortkey = 'sku'
 
     if request.GET:
 

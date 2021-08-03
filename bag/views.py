a37ab30496606b3ccce363b9800bd457197e6de3
Gertobin11 <gertobin11@gmail.com>
2021-08-03 23:27:11 +0000

    added checkout app and listeners and signals

--- bag/views.py
+++ bag/views.py
@@ -98,5 +98,5 @@ def remove_from_bag(request, item_id):
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
-        message.error(request, f'error removing item: {e}')
+        messages.error(request, f'error removing item: {e}')
         return HttpResponse(status=500)

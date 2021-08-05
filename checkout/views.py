c0e44bd64fdbd4969faf81f825e37e98d47fdbe2
Gertobin11 <gertobin11@gmail.com>
2021-08-05 15:29:08 +0000

    added checkout stripe logic

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,44 @@
+
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 'There is currently no items in your bag')
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
+    print(stripe.api_key)
+    
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request,
+                         'did you forget to set stripe public key in settings')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JKobYIGPHoH1QniNlPnhqpgPmxJr2BYs1nt4lxXIOcW1JBy8WRqdJlYeAt3tFZ9iXW2ux2OEBVQvmNBsH59QonG00Qb8OphmZ',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret
     }
 
     return render(request, template, context)

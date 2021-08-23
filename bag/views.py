d835286928da7c4427203c1c426fe2984cd27340
Gertobin11 <gertobin11@gmail.com>
2021-08-23 19:29:56 +0000

    loaded processing payment overlay with spinning record icon

--- bag/views.py
+++ bag/views.py
@@ -88,7 +88,8 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(request, f'Updated {product.name} ' /
+                         f'quantity to your {bag[item_id]}')
         else:
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
